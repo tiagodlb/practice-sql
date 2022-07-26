097ac00e0e52f6ad438a0c9a0bebce48dec6d1b4
Tiago <tigolimab223@gmail.com>
2022-07-26 20:50:36 -0300

    even better

--- questoes/q4.sql
+++ questoes/q4.sql
@@ -1,2 +1,2 @@
 --Exercicio 4
-SELECT users.id, t1.name, t3.name AS "role", t4.name AS "company", t2."startDate" FROM users JOIN users AS t1 ON users.id = 50 JOIN experiences AS t2 ON t2."userId" = t1.id JOIN roles AS t3 ON t1.id = 50 AND t3.id = t2."roleId" JOIN companies AS t4 ON t2."companyId" = t4.id WHERE t2."endDate" IS NOT NULL;
+SELECT users.id, t1.name, t3.name AS "role", t4.name AS "company", t2."startDate" FROM users JOIN users AS t1 ON users.id = 50 JOIN experiences AS t2 ON t2."userId" = t1.id JOIN roles AS t3 ON t1.id = 50 AND t3.id = t2."roleId" JOIN companies AS t4 ON t2."companyId" = t4.id WHERE t2."endDate" IS NULL;
